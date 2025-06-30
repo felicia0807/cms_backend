0ba1601585636f44240eb556f6236acc2df02a8b
“feliciachan_2000” <feliciachan_2000@hotmail.com>
2025-06-30 13:40:08 +0800

    fix

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,18 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:5173"],
+      credentials: true,
+    },
+  },
+  "strapi::security",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
